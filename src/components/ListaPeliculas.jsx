c23879462bc95497416d0a0e5608fcb9d96696c9
Nheil3Dev <98476350+Nheil3Dev@users.noreply.github.com>
2023-05-16 11:56:53 +0200

    fix array of movies on mobile

--- src/components/ListaPeliculas.jsx
+++ src/components/ListaPeliculas.jsx
@@ -36,32 +36,64 @@ export function ListaPeliculasBuscador ({ peliculas }) {
 
 export function ListaPeliculasUsuario ({ peliculas, isUser }) {
   const [width, setWidth] = useState(window.innerWidth)
-  const anchoPantalla = Math.floor(((width * 80 / 100) - 40) / 220)
+  const [anchoPantalla, setAnchoPantalla] = useState(Math.floor(((width * 80 / 100) - 40) / 220))
   const [ultimaPeli, setUltimaPeli] = useState(anchoPantalla)
-  const [peliculasFiltro, setPeliculasFiltro] = useState([])
+  const [primeraPeli, setPrimeraPeli] = useState(0)
+  const [peliculasFiltro, setPeliculasFiltro] = useState(peliculas.slice(primeraPeli, ultimaPeli))
   const [visibleRight, setVisibleRight] = useState(true)
   const [visibleLeft, setVisibleLeft] = useState(false)
   const styleButtonRight = visibleRight ? 'more-button' : 'more-button hidden'
   const styleButtonLeft = visibleLeft ? 'more-button' : 'more-button hidden'
+  console.log('primera Peli: ', primeraPeli)
+  console.log('ultima Peli: ', ultimaPeli - 1)
+  console.log('ancho array: ', anchoPantalla)
 
   useEffect(() => {
-    setPeliculasFiltro(peliculas.slice(ultimaPeli - anchoPantalla, ultimaPeli))
-    const handleResize = () => {
-      setWidth(window.innerWidth)
+    const handleResize = (event) => {
+      console.log(event.currentTarget.innerWidth)
+      const newWidth = event.currentTarget.innerWidth
+      setWidth(newWidth)
+      const newAnchoPantalla = Math.floor(((newWidth * 75 / 100) - 40) / 220)
+      setAnchoPantalla(newAnchoPantalla)
+      if (peliculas.length - primeraPeli < newAnchoPantalla) {
+        const newUltimaPeli = peliculas.length
+        setUltimaPeli(newUltimaPeli)
+        const newPrimeraPeli = newUltimaPeli - newAnchoPantalla
+        setPrimeraPeli(newPrimeraPeli)
+        const newPeliculas = peliculas.slice(newPrimeraPeli, newUltimaPeli)
+        setPeliculasFiltro(newPeliculas)
+        console.log('hola')
+        if (peliculas.length - newUltimaPeli === 0) {
+          setVisibleRight(false)
+        }
+      } else {
+        const newUltimaPeli = primeraPeli + newAnchoPantalla
+        setUltimaPeli(newUltimaPeli)
+        const newPeliculas = peliculas.slice(primeraPeli, newUltimaPeli)
+        setPeliculasFiltro(newPeliculas)
+        if (peliculas.length - newUltimaPeli > 0) {
+          setVisibleRight(true)
+        } else {
+          setVisibleRight(false)
+        }
+      }
     }
     window.addEventListener('resize', handleResize)
     return () => {
       window.removeEventListener('resize', handleResize)
     }
-  }, [ultimaPeli, width])
+  }, [width])
 
   const handleClick = () => {
     if (peliculas.length > ultimaPeli) {
       if (visibleLeft === false) {
         setVisibleLeft(true)
       }
-      setUltimaPeli(ultimaPeli + 1)
-      setPeliculasFiltro(peliculas.slice(ultimaPeli - anchoPantalla, ultimaPeli))
+      const newUltimaPeli = ultimaPeli + 1
+      const newPrimeraPeli = primeraPeli + 1
+      setUltimaPeli(newUltimaPeli)
+      setPrimeraPeli(newPrimeraPeli)
+      setPeliculasFiltro(peliculas.slice(newPrimeraPeli, newUltimaPeli))
     }
     if (peliculas.length - ultimaPeli === 1) {
       setVisibleRight(false)
@@ -73,11 +105,14 @@ export function ListaPeliculasUsuario ({ peliculas, isUser }) {
       if (visibleRight === false) {
         setVisibleRight(true)
       }
-      setUltimaPeli(ultimaPeli - 1)
-      setPeliculasFiltro(peliculas.slice(ultimaPeli - anchoPantalla, ultimaPeli))
-    }
-    if (ultimaPeli - anchoPantalla === 1) {
-      setVisibleLeft(false)
+      const newUltimaPeli = ultimaPeli - 1
+      const newPrimeraPeli = primeraPeli - 1
+      setUltimaPeli(newUltimaPeli)
+      setPrimeraPeli(newPrimeraPeli)
+      setPeliculasFiltro(peliculas.slice(newPrimeraPeli, newUltimaPeli))
+      if (newPrimeraPeli === 0) {
+        setVisibleLeft(false)
+      }
     }
   }
 
